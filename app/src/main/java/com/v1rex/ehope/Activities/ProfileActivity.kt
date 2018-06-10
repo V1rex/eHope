89338e14ae2d2d55eba63a65c1a2f422d9d75869
Mohamed Amine Bahij <go4computergeek@gmail.com>
2018-06-10 23:06:07 +0000

    Added the login test from firebase

--- app/src/main/java/com/v1rex/ehope/Activities/ProfileActivity.kt
+++ app/src/main/java/com/v1rex/ehope/Activities/ProfileActivity.kt
@@ -5,20 +5,30 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v7.app.ActionBarDrawerToggle
+import android.text.TextUtils
 import android.view.MenuItem
 import android.view.WindowManager
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.v1rex.ehope.R
 import com.v1rex.ehope.R.id.*
+import kotlinx.android.synthetic.main.activity_login_register.*
 import kotlinx.android.synthetic.main.activity_profile.*
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+
+
 
 class ProfileActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+    var mAuth : FirebaseAuth? = null
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         var id = item.itemId
 
         when(id){
             R.id.test_abilities -> startActivity(Intent(this, TestAbilitiesActivity::class.java))
-            R.id.sensibilisation -> Toast.makeText(this, "This sensi", Toast.LENGTH_SHORT).show()
+            R.id.sensibilisation -> startActivity(Intent(this, SensibilisationActivity::class.java))
             R.id.nearby_center -> startActivity(Intent(this, NearbyCenterActivity::class.java))
            R.id.logout -> startActivity(Intent(this, MainActivity::class.java))
         }
@@ -30,6 +40,8 @@ class ProfileActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelec
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        mAuth = FirebaseAuth.getInstance()
 
         val navigationView = findViewById<NavigationView>(R.id.navigation_view)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
@@ -58,6 +70,57 @@ class ProfileActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelec
         }
     }
 
+    private fun login(){
+        if (mAuth == null) {
+            return
+        }
+
+        var cancel = false
+
+        var email = user_register_edit_text.text.toString()
+        var password = password_register_edit_text.text.toString()
+
+        if (!TextUtils.isEmpty(password) ) {
+            // TODO add error to the layout
+            cancel = true
+        } else if(!isPasswordValid(password)){
+            // TODO add error to the layout
+            cancel = true
+        } else{
+            cancel = false
+        }
+
+        if (TextUtils.isEmpty(email)) {
+            // TODO add error to the layout
+            cancel = true
+        } else if (!isEmailValid(email)) {
+            // add error to the layout
+            cancel = true
+        } else {
+            cancel = false
+        }
+
+        if(!cancel){
+            // TODO add the progress layout
+
+            mAuth!!.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+                // TODO set the progress layout visibility to GONE
+
+                if (!task.isSuccessful) {
+                    // TODO if task isn't successfully add error to the layout
+
+                } else {
+
+                    // TODO start activity
+                    finish()
+
+                }
+            }
+
+        }
+
+    }
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         if(drawerToggle!!.onOptionsItemSelected(item)){
             return true
@@ -65,6 +128,14 @@ class ProfileActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelec
 
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        return email.contains("@")
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        return password.length > 4
     }
 
 
