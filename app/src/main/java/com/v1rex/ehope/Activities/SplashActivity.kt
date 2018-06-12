de62daea87306848fdb80f4a63eb413d6024aa42
Mohamed Amine Bahij <go4computergeek@gmail.com>
2018-06-12 17:06:31 +0000

    Added user check if he is connected to splash activity

--- app/src/main/java/com/v1rex/ehope/Activities/SplashActivity.kt
+++ app/src/main/java/com/v1rex/ehope/Activities/SplashActivity.kt
@@ -3,13 +3,18 @@ package com.v1rex.ehope.Activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this,MainActivity::class.java)
-        startActivity(intent)
+
+        if(FirebaseAuth.getInstance().currentUser != null){
+            startActivity(Intent(this, ProfileActivity::class.java))
+        } else if (FirebaseAuth.getInstance().currentUser == null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
         finish()
     }
 }
